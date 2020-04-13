bac3c49be437a9a6333349c85991bc79232e1e1e
cxz5309 <cxz5309@gmail.com>
2020-04-13 13:25:46 +0900

    방금전에 형상관리 안해서 큰일날뻔해서 바로 올림 현재 undo, redo 제작중, 문제 해결 이전상태 OnClickUp상태에서 저장하고 그걸 바로 undo시킨것이 문제 가장 첫 상태부터 저장하고 그 이후부터 undo가 가능하도록 변경 예정

---
+++ Paint Adventure 2_Paint Tutorial/Assets/Painter/PaintV2/Script/PaintCanvas2Demo/PaintCanvas2UGUIController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaintCanvas2UGUIController : MonoBehaviour {
+
+    [Header("Setting")]
+    public ToggleGroup leftBar;
+    public ToggleGroup rightBar;
+    public Slider penScaleBar;
+    public Toggle isEarseBar;
+    public Toggle isColorfulBar;
+
+    [Header("Paint Canvas")]
+    public Painter painterCanvas;
+
+    private bool _isMouseDown =false;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+		bool isRawImg = false;
+		foreach(Toggle t in leftBar.ActiveToggles()){
+
+			//Set pen texture.
+			painterCanvas.penMat.mainTexture = t.GetComponent<Image>().sprite.texture;
+
+
+			PenSetting ps = t.GetComponent<PenSetting>();
+
+			//save raw img flag.
+			isRawImg = ps.isRawImg;
+			break;
+		}
+
+		if(!isColorfulBar.isOn){
+			//Set the pen color if is not colorfull stuatus.
+			if(isRawImg || isColorfulBar.isOn){
+				painterCanvas.penColor = Color.white;
+			}else{
+				foreach(Toggle t in rightBar.ActiveToggles()){
+					painterCanvas.penColor = t.GetComponent<Image>().color;
+					break;
+				}
+			}
+		}
+
+		//pen size.
+		painterCanvas.brushScale = penScaleBar.value;
+
+		//is erase.
+		painterCanvas.isErase = isEarseBar.isOn;
+
+		//draw line or draw colorful line.
+		painterCanvas.paintType = isColorfulBar.isOn? Painter.PaintType.DrawColorfulLine: Painter.PaintType.DrawLine;
+	}
+
+	void Update(){
+		if(Input.GetMouseButtonDown(0)){
+			_isMouseDown = true;
+            //Draw once when mouse down.
+
+            Vector2 pos;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(painterCanvas.transform as RectTransform, Input.mousePosition, null, out pos))
+            {
+                if (painterCanvas.GetComponent<RectTransform>().rect.Contains(pos))
+                {
+                    painterCanvas.CopyTexture(true);
+                }
+                if (painterCanvas.GetComponent<RectTransform>().rect.Contains(pos))
+                {
+                    painterCanvas.ClickDraw(pos, null, painterCanvas.penMat.mainTexture, painterCanvas.brushScale,
+                       painterCanvas.penMat, painterCanvas.renderTexture, true);
+                }
+            }
+        }
+		else if(Input.GetMouseButton(0)){
+			if(_isMouseDown)
+			{
+				//draw on mouse drag.
+                Vector2 pos;
+                if (RectTransformUtility.ScreenPointToLocalPointInRectangle(painterCanvas.transform as RectTransform, Input.mousePosition, null, out pos))
+                {
+                    if (painterCanvas.GetComponent<RectTransform>().rect.Contains(pos))
+                    {
+                        painterCanvas.Drawing(pos, null, painterCanvas.renderTexture, false, true);
+                    }
+                }
+			}
+		}
+		else if(Input.GetMouseButtonUp(0) && _isMouseDown)
+		{
+            Vector2 pos;
+
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(painterCanvas.transform as RectTransform, Input.mousePosition, null, out pos))
+            {
+                
+            }
+            painterCanvas.EndDraw();
+			_isMouseDown = false;
+		}
+	}
+
+	public void OnClickClearCanvas(){
+		//clear the paint canvas.
+		painterCanvas.ClearCanvas();
+	}
+
+    public void OnClickSaveTexture()
+    {
+        painterCanvas.SaveTexture();
+    }
+
+    public void OnClickUndoTexture()
+    {
+        painterCanvas.UndoTexture();
+    }
+
+    public void OnClickRedoTexture()
+    {
+        painterCanvas.RedoTexture();
+    }
+}
